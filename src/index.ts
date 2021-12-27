98de4ddf909645f4ce0ce6803e9e8249b36337cb
Gabriel Souza <gabriel@4bi.com.br>
2021-12-26 23:14:41 -0300

    refactoring route status

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express from 'express';
+import statusRoute from './routes/status.route';
 import usersRoute from './routes/users.route';
 
 const app = express();
 //consfiguation application
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //consfiguation router
+app.use(statusRoute);
 app.use(usersRoute);
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Sucesso' });
-});
 
 //server startup
 app.listen(3000, () => {
