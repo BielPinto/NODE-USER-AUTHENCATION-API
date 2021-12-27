d5d7d0958aca35f2fecf02a245a25b3613073e75
Gabriel Souza <gabriel@4bi.com.br>
2021-12-26 22:25:34 -0300

    implementing route by user id

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express'
+import usersRoute from './routes/users.route';
 
 const app = express();
 
+app.use(usersRoute);
+
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({ foo: 'Sucesso' });
-
-})
+});
 
 app.listen(3000, () => {
-    console.log("applicação rodando na 3000")
+    console.log("applicação rodando na 3000");
 })
