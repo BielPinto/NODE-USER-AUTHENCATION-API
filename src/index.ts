a31602d3617e5c9fdeef673590bbf2127d7f9705
Gabriel Souza <gabriel@4bi.com.br>
2021-12-26 22:49:15 -0300

    Developing and testing method post

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import express, { Request, Response, NextFunction } from 'express'
 import usersRoute from './routes/users.route';
 
 const app = express();
+//consfiguation application
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
+//consfiguation router
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({ foo: 'Sucesso' });
 });
 
+//server startup
 app.listen(3000, () => {
     console.log("applicação rodando na 3000");
 })
